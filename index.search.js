677467232e13f3e89e947676831d7453ebddd655
LiuShuojiang <Leo2001@stu.xjtu.edu.cn>
2023-02-25 17:47:13 +0800

    Generate static files

---
+++ index.search.js
@@ -0,0 +1,23 @@
+var relearn_search_index = [
+  {
+    "content": "Shuojiang Liu Undergraduate Student, Xi’an Jiaotong University\n_Email: Leo2001@stu.xjtu.edu.cn; leoshuojiang@gmail.com\nAbout Me Shuojiang Liu is an undergraduate student from Xi’an Jiaotong University (XJTU), majoring in automation at Qian Xuesen Honors College (administrative) and School of Automation Science and Engineering (professional). Starting in December 2022, he has participated in the Non-Graduate Non-Exchange (NGNE) program organized by School of Computing (SoC) at National University of Singapore (NUS), where he is conducting computer science research related to Human-Computer Interaction (HCI) under the supervision of Prof. Yi-Chieh Lee.\nResearch Interests Graph Neural Networks, Reinforcement Learning, Image Processing, Computational Biology, Human-Computer Interaction, Chatbot Design, Natural Language Processing…\nEducation Xi’an Jiaotong University (XJTU), China\nHonors Bachelor Degree in Automation Science and Engineering Qian Xuesen Honors College August 2019 ~ June 2023 Supervisor: Liangjun Ke National University of Singapore (NUS), Singapore\nNon-Graduate Non-Exchange (NGNE) Program (with Undergraduate Dissertation) School of Computing (SoC) December 2022 ~ May 2023 Supervisor: Yi-Chieh Lee ",
+    "description": "",
+    "tags": null,
+    "title": "Shuojiang Liu's Personal Website [Beta]",
+    "uri": "/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
